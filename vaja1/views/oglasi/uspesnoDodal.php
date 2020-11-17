ccde6b4bd511d59d1bdacc3d178ba6ddf4a63c0a
mx2-m <74437844+mx2-m@users.noreply.github.com>
2020-11-17 17:35:42 +0100

    Delete uspesnoDodal.php

--- vaja1/views/oglasi/uspesnoDodal.php
+++
@@ -1,5 +0,0 @@
-<!--enostaven pogled za informiranje o uspešnosti dodajanja oglasa -->
-<!-- ta se nahaja v spremenljivki $oglas, ki smo jo pripravili v kontrolerju, uporabniku pa damo še povezavo na izpis celotnega oglasa (preko akcije prikazi) -->
-
-<p>Oglas je bil uspešno shranjen!</p>
-<p>Viden je <a href="?controller=oglasi&action=prikazi&id=<?php echo $oglas->id; ?>">tukaj</a></p>
